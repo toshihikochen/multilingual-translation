e62f48a5fef93efd07bf62b9acb944f71d2557c4
gdfscjy <toshihikochen@qq.com>
2023-07-10 15:07:38 +0800

    add parameter val_data_paths

--- pl_models/matrix_data.py
+++ pl_models/matrix_data.py
@@ -2,23 +2,35 @@ import torch
 from torch.utils.data import ConcatDataset, random_split, DataLoader
 import lightning as pl
 import transformers
+import warnings
 
 from datasets import ParquetMatrix
 
 
 class MatrixDataModule(pl.LightningDataModule):
-    def __init__(self, train_data_paths, tokenizer, test_data_paths=None, batch_size=4, val_split=None,
-                 specific_lang_pair=None, use_augment=False):
+    def __init__(self, train_data_paths, tokenizer, val_split=None, val_data_paths=None, test_data_paths=None,
+                 batch_size=4, specific_lang_pair=None, use_augment=False):
         super().__init__()
         self.train_data_paths = train_data_paths
-        self.test_data_paths = test_data_paths
         self.tokenizer = tokenizer
-        self.batch_size = batch_size
+        # if val_split is None, use the full train dataset
+        # if val_split is a float, use the ratio of the train dataset
+        # if val_split is an int, use the number of samples of the train dataset
         self.val_split = val_split
+        # if val_data_paths is not None, then use the val_data_paths as the validation dataset
+        self.val_data_paths = val_data_paths
+        self.test_data_paths = test_data_paths
+        self.batch_size = batch_size
         self.specific_lang_pair = specific_lang_pair
         self.use_augment = use_augment
 
-        self.lang_pairs = []    # [(src, tgt), ...]
+        # if val_data_paths is not None, ignore val_split
+        if self.val_data_paths:
+            warnings.warn('val_data_paths is not None, ignore val_split')
+            self.val_split = None
+
+        # get the language pairs
+        self.lang_pairs = []    # [(src1, tgt1), ...]
         if self.specific_lang_pair:
             self.lang_pairs = [specific_lang_pair]
         else:
@@ -43,6 +55,7 @@ class MatrixDataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
+            # load the train dataset from path list
             parquet_datasets = [
                 ParquetMatrix(
                     data_path, tokenizer=self.tokenizer, use_augment=self.use_augment,
@@ -51,16 +64,27 @@ class MatrixDataModule(pl.LightningDataModule):
                 for data_path in self.train_data_paths
             ]
             dataset = ConcatDataset(parquet_datasets)
-            if not self.val_split:
-                # full train
-                self.train_dataset = dataset
-            else:
+            # split the train dataset into train and val
+            if self.val_split:
                 if isinstance(self.val_split, float):
                     val_size = int(self.val_split * len(dataset))
                 else:
                     val_size = self.val_split
                 train_size = len(dataset) - val_size
                 self.train_dataset, self.val_dataset = random_split(dataset, [train_size, val_size])
+            else:
+                # full train
+                self.train_dataset = dataset
+                # load the val dataset from path list if specified
+                if self.val_data_paths:
+                    parquet_datasets = [
+                        ParquetMatrix(
+                            data_path, tokenizer=self.tokenizer, use_augment=False,
+                            specific_lang_pair=self.specific_lang_pair
+                        )
+                        for data_path in self.val_data_paths
+                    ]
+                    self.val_dataset = ConcatDataset(parquet_datasets)
 
         if stage == 'test' or stage is None:
             if self.test_data_paths:
